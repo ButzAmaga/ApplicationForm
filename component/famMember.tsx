d15c6c691f496fce69c49418fb5ff8348f12a097
Butch Amaga Imperial <butchamagaimperial2003@gmail.com>
2026-06-06 15:39:39 +0800

    ch: updated the metadata of layout and added hint NA to fam number field

--- component/famMember.tsx
+++ component/famMember.tsx
@@ -108,6 +108,7 @@ export function Step4Family({ errors, show }: Step4Props) {
                   name={`family_members_${idx}_phone`}
                   errors={[]}
                   placeholder="+63993xxxxx"
+                  hint="N/A if none"
                 />
 
                 <CheckboxGroup
